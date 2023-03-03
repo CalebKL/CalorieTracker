c46ad7762b4c3438d93cf34ea9295a89a67eccd6
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-03-03 08:57:00 +0300

    Added welcome screen

--- app/src/main/java/com/example/calorietracker/MainActivity.kt
+++ app/src/main/java/com/example/calorietracker/MainActivity.kt
@@ -11,33 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calorietracker.ui.theme.CalorieTrackerTheme
+import com.example.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalorieTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                WelcomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalorieTrackerTheme {
-        Greeting("Android")
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ buildscript {
         classpath(Build.androidBuildTools)
         classpath(Build.hiltAndroidGradlePlugin)
         classpath(Build.kotlinGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
