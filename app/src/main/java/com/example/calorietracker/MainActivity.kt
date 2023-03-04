496130832ff7606f49f709159801e1d058990b25
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-03-04 08:06:32 +0300

    Created AgeViewModel,AgeScreen, FilteringOutDigits and UiText

--- app/src/main/java/com/example/calorietracker/MainActivity.kt
+++ app/src/main/java/com/example/calorietracker/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,6 +14,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.calorietracker.navigation.navigate
 import com.example.calorietracker.ui.theme.CalorieTrackerTheme
 import com.example.core.navigation.Route
+import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,39 +26,49 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalorieTrackerTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination =Route.WELCOME
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
                 ){
-                    composable(Route.WELCOME){
-                        WelcomeScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.AGE){
+                    val unUsedPadding = it
+                    NavHost(
+                        navController = navController,
+                        startDestination =Route.WELCOME
+                    ){
+                        composable(Route.WELCOME){
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE){
+                            AgeScreen(
+                                onNavigate = navController::navigate,
+                                scaffoldState = scaffoldState
+                            )
+                        }
+                        composable(Route.GENDER){
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.HEIGHT){
 
-                    }
-                    composable(Route.GENDER){
-                        GenderScreen(onNavigate = navController::navigate)
-                    }
-                    composable(Route.HEIGHT){
+                        }
+                        composable(Route.WEIGHT){
 
-                    }
-                    composable(Route.WEIGHT){
+                        }
+                        composable(Route.NUTRIENT_GOAL){
 
-                    }
-                    composable(Route.NUTRIENT_GOAL){
+                        }
+                        composable(Route.ACTIVITY){
 
-                    }
-                    composable(Route.ACTIVITY){
+                        }
+                        composable(Route.GOAL){
 
-                    }
-                    composable(Route.GOAL){
+                        }
+                        composable(Route.TRACKER_OVERVIEW){
 
-                    }
-                    composable(Route.TRACKER_OVERVIEW){
+                        }
+                        composable(Route.SEARCH){
 
-                    }
-                    composable(Route.SEARCH){
-
+                        }
                     }
                 }
             }
--- core/src/main/java/com/example/core/util/UiEvent.kt
+++ core/src/main/java/com/example/core/util/UiEvent.kt
@@ -3,4 +3,6 @@ package com.example.core.util
 sealed class UiEvent{
     data class Navigate(val route:String):UiEvent()
     object NavigateUp:UiEvent()
+
+    data class ShowSnackbar(val message:UiText):UiEvent()
 }
