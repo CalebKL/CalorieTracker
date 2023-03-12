aa272808548d8ab429ca191e0955f175aa0293e2
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-03-12 21:58:16 +0300

    created trackedFoodEntity

---
+++ tracker/tracker_data/src/main/java/com/example/tracker_data/local/entity/TrackedFoodEntity.kt
@@ -0,0 +1,19 @@
+package com.example.tracker_data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class TrackedFoodEntity(
+    val name:String,
+    val carbs: Int,
+    val protein:Int,
+    val fat:Int,
+    val imageUrl: String?,
+    val type:String,
+    val dayOfMonth:Int,
+    val month:Int,
+    val year: Int,
+    val calories:Int,
+    @PrimaryKey val id: Int? = null
+)
