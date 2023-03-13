04d32ddeb751a91f4d1b5acce23c723c80604479
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-03-13 14:06:03 +0300

    Added trackerDao, TrackerDb and added DI for providing the database

--- tracker/tracker_data/src/main/java/com/example/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/example/tracker_data/di/TrackerDataModule.kt
@@ -1,5 +1,8 @@
 package com.example.tracker_data.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.tracker_data.local.TrackerDatabase
 import com.example.tracker_data.remote.OpenFoodApi
 import dagger.Module
 import dagger.Provides
@@ -36,6 +39,15 @@ object TrackerDataModule {
             .client(client)
             .build()
             .create()
+    }
+    @Provides
+    @Singleton
+    fun providesTrackerDatabase(app:Application):TrackerDatabase{
+        return Room.databaseBuilder(
+            app,
+            TrackerDatabase::class.java,
+            "tracker_db"
+        ).build()
     }
 }
 
