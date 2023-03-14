e4c90df587ddefd498a304e7734fd0dbf3def03b
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-03-14 13:22:41 +0300

    Added TrackerOverviewState.kt and viewmodel and updated preferences and DI

--- app/src/main/java/com/example/calorietracker/di/AppModule.kt
+++ app/src/main/java/com/example/calorietracker/di/AppModule.kt
@@ -3,7 +3,7 @@ package com.example.calorietracker.di
 import android.app.Application
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
-import com.example.core.domain.DefaultPreferences
+import com.example.core.data.preferences.DefaultPreferences
 import com.example.core.domain.preferences.Preferences
 import com.example.core.domain.use_case.FilterOutDigits
 import dagger.Module
